54e2640892f6d025a245fecbdeae4a70dd033aad
Jeremy Bugay <jeremybugay@gmail.com>
2022-04-06 11:54:33 -0400

    Added Chuck Norris dependency. Implemented JokeService

---
+++ src/main/java/jdb/springframework/jokesappspring5/services/JokeService.java
@@ -0,0 +1,6 @@
+package jdb.springframework.jokesappspring5.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/jdb/springframework/jokesappspring5/services/JokeServiceImpl.java
@@ -0,0 +1,20 @@
+package jdb.springframework.jokesappspring5.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService{
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+
+
+}
